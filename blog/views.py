ea003948882b9689440450b82c15ad1da12be451
Aigerim <maviboncuaika@gmail.com>
2022-04-30 11:22:23 +0600

    molchanov5

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,15 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post, Tag
 
 def posts_list(request):
-    n = ['Oleg', 'kkkk', 'ksu']
-    return render(request, 'blog/index.html', context={'names': n})
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_detail.html', context={'post': post})
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
